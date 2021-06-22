5295e449cc8c1f44ab909b8647b2a8e490d2981c
novaedra <novaedra@gmail.com>
2021-06-22 12:02:27 +0200

    add page title

--- src/Template/Screen/IdeaBoxScreen.js
+++ src/Template/Screen/IdeaBoxScreen.js
@@ -5,6 +5,7 @@ import Button from '@material-ui/core/Button';
 export function IdeaBoxScreen() {
     return (
         <>
+            <h2 className={"pageTitle"}>Boîte à idées</h2>
         <div className={'ideaForm'}>
             <form className={'ideaBox'} noValidate autoComplete="off">
                 <TextField className="input" id="ideaFormTitle" label="TITRE"/><br/>
