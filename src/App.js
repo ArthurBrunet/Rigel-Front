808fe75d9ecfb7292424e565bdb72c50eaf884cc
novaedra <novaedra@gmail.com>
2021-07-07 15:23:44 +0200

    refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,18 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, {useState} from "react";
+import {BrowserRouter as Router, Switch} from "react-router-dom";
 import "./Style/Normalize.css";
 import "./Style/App.css";
-import { HomeScreen } from "./Template/Screen/HomeScreen";
-import { LoginScreen } from "./Template/Screen/LoginScreen";
-import { DirectoryScreen } from "./Template/Screen/DirectoryScreen";
-import { ChatScreen } from "./Template/Screen/ChatScreen";
-import { IdeaBoxScreen } from "./Template/Screen/IdeaBoxScreen";
-import { Header } from "./Template/Component/Header";
-import { RegisterScreen } from "./Template/Screen/RegisterScreen";
+import {AuthProvider, useAuth} from "./Store/Auth/auth"
+import store from './Store/configStore';
+import {Provider} from 'react-redux';
+import RouteConfig from './Config/Route';
 
 export default function App() {
-  // const [isLogged, setIsLogged] = useState(false)
-  return (
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path="/">
-            <HomeScreen />
-          </Route>
-          <Route exact path="/talk">
-            <ChatScreen />
-          </Route>
-          <Route exact path="/directory">
-            <DirectoryScreen />
-          </Route>
-          <Route exact path="/ideabox">
-            <IdeaBoxScreen />
-          </Route>
-          <Route exact path="/login">
-            <LoginScreen />
-          </Route>
-          <Route exact path="/register">
-            <RegisterScreen />
-          </Route>
-        </Switch>
-      </Router>
-  );
+    return (
+        <Provider store={store}>
+            <AuthProvider>
+                <RouteConfig/>
+            </AuthProvider>
+        </Provider>
+    );
 }
