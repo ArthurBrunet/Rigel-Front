3c6c889e2bf67fc325f64fae66299d04ba563af0
novaedra <novaedra@gmail.com>
2021-07-08 16:55:15 +0200

    remove unuse import

--- src/Template/Component/CardArticle.js
+++ src/Template/Component/CardArticle.js
@@ -1,52 +1,31 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles({
-    root: {
-        float: "left",
-        maxWidth: 354,
-        minWidth: 150,
-        margin: '3%',
-    },
-});
-
-export default function ImgMediaCard() {
-    const classes = useStyles();
+export default function ImgMediaCard(item) {
+    item = item.item;
 
     return (
         <div className="cardArticle">
             <div className="card">
-                <a href="#">
                     <div className="card-image">
-                        <img src="https://cdn.pixabay.com/photo/2015/01/08/18/27/startup-593341_960_720.jpg"
+                        <img src={item.mediaUrl}
                              alt="Orange"/>
                     </div>
 
                     <div className="card-body">
 
                         <div className="card-date">
-                            <time>20 Novembre 1992</time>
+                            <time>{item.datePost}</time>
                         </div>
 
                         <div className="card-title">
-                            <h3>Lorem Ipsum</h3>
+                            <h3>{item.title}</h3>
                         </div>
 
                         <div className="card-excerpt">
-                            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam voluptatibus autem
-                                consectetur voluptate facere at, omnis ab optio placeat officiis! Animi modi harum enim quia
-                                veniam consectetur unde autem inventore.</p>
+                            <p>{item.content}</p>
                         </div>
 
                     </div>
-                </a>
             </div>
         </div>
 
